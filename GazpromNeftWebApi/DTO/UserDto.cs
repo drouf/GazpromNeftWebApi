513be71e46f8c64648bd810c357b933ce301f96f
Владимир Чепик <mr.drouf777@outlook.com>
2023-12-10 17:37:03 +0300

    create dto for user

---
+++ GazpromNeftWebApi/DTO/UserDto.cs
@@ -0,0 +1,12 @@
+﻿namespace GazpromNeftWebApi.DTO
+{
+    public class UserDto
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Patronymic { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+    }
+}
